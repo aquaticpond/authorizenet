4eacbbc3a1ffd257192979223828a04c72490122
Aquatic Pond <etheriad@gmail.com>
2016-05-26 16:32:23 -0400

    Create an address

---
+++ src/Aquatic/AuthorizeNet/CIM/CreateCustomerShippingAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Aquatic\AuthorizeNet\CIM;
+
+use Aquatic\AuthorizeNet\Request;
+use Aquatic\AuthorizeNet\Contract\Address;
+
+class CreateCustomerShippingAddress extends Request
+{
+    protected $_soapMethod = 'CreateCustomerShippingAddress';
+    protected $_wsdl = 'CIM';
+
+    public function __construct(int $profile_id, Address $address)
+    {
+        $this->_request = [
+            'customerProfileId' => $profile_id,
+            'address' => [
+                'firstName' => $address->getFirstName(),
+                'lastName'  => $address->getLastName(),
+                'company'   => $address->getCompany(),
+                'address'   => $address->getStreet(),
+                'city'      => $address->getCity(),
+                'state'     => $address->getState(),
+                'zip'       => $address->getZip(),
+                'country'   => $address->getCountry(),
+                'phoneNumber'=> $address->getPhoneNumber(),
+            ],
+        ];
+
+        $this->setValidationMode('none');
+    }
+}
