d1aa7fd2c6eb1e915842a1d1999d1eadeb3646d0
Aquatic Pond <etheriad@gmail.com>
2016-06-01 14:26:17 -0400

    Type hints

--- src/Aquatic/AuthorizeNet/CreditCard.php
+++ src/Aquatic/AuthorizeNet/CreditCard.php
@@ -9,7 +9,7 @@ class CreditCard implements Contract
     public static $expiration_date_format = '\d{2}-\d{4}';
     public $number, $cvv, $expiration_date;
 
-    public function __construct(int $number, int $cvv, int $expiration_date)
+    public function __construct(int $number, int $cvv, string $expiration_date)
     {
         if(!preg_match(static::$expiration_date_format, $expiration_date))
             throw new \InvalidArgumentException("Expiration date must be in mm-yyyy format");
@@ -20,17 +20,17 @@ class CreditCard implements Contract
     }
 
 
-    public function getNumber()
+    public function getNumber(): int
     {
         return $this->number;
     }
 
-    public function getCVV()
+    public function getCVV(): int
     {
         return $this->cvv;
     }
 
-    public function getExpirationDate()
+    public function getExpirationDate(): string
     {
         return $this->expiration_date;
     }
